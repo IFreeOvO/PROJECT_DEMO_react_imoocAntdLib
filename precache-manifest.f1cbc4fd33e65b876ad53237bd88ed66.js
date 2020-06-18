1c27b15aba46dde539ca5791738c51e744f0a496
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-18 15:01:59 +0000

    Deploy IFreeOvO/PROJECT_DEMO_react_imoocAntdLib to github.com/IFreeOvO/PROJECT_DEMO_react_imoocAntdLib.git:gh-pages

---
+++ precache-manifest.f1cbc4fd33e65b876ad53237bd88ed66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b6a77a83bc2d197048dda22cb155aab",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.57fc62112a3d94dc8cc4.bundle.js"
+  },
+  {
+    "url": "runtime~main.57fc62112a3d94dc8cc4.bundle.js"
+  },
+  {
+    "revision": "9e8c076cab28d4904dd2",
+    "url": "static/css/main.7a992d78.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.57fc62112a3d94dc8cc4.bundle.js"
+  },
+  {
+    "url": "vendors~main.57fc62112a3d94dc8cc4.bundle.js.LICENSE.txt"
+  }
+]);
